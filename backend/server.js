82dc86e21c119765352aaf5a838bc0c4ca39162a
KenjaminButton <kennethpchang@gmail.com>
2023-09-03 02:36:14 +0800

    Deleted a book with Mongoose

--- backend/server.js
+++ backend/server.js
@@ -13,34 +13,6 @@ app.get('/', (req, res) => {
   return res.status(234).send('Welcome to Kenjamin Button land')
 })
 
-// Route to POST a new book
-// http://localhost:5555/books
-app.post('/books', async (request, response) => {
-  try {
-    if (
-      !request.body.title ||
-      !request.body.author ||
-      !request.body.publishYear
-    ) {
-      return response.status(400).send({
-        message: 'Send all required fields: title, author, publishYear'
-      })
-    }
-    const newBook = {
-      title: request.body.title,
-      author: request.body.author,
-      publishYear: request.body.publishYear
-    }
-
-    const book = await Book.create(newBook)
-    return response.status(201).send(book)
-
-  } catch (error) {
-    console.log(error.message)
-    response.status(500).send({message: error.message})
-  }
-})
-
 // Route to GET all books
 // http://localhost:5555/books
 app.get('/books', async (request, response) => {
@@ -70,6 +42,34 @@ app.get('/books/:id', async (request, response) => {
   }
 })
 
+// Route to POST a new book
+// http://localhost:5555/books
+app.post('/books', async (request, response) => {
+  try {
+    if (
+      !request.body.title ||
+      !request.body.author ||
+      !request.body.publishYear
+    ) {
+      return response.status(400).send({
+        message: 'Send all required fields: title, author, publishYear'
+      })
+    }
+    const newBook = {
+      title: request.body.title,
+      author: request.body.author,
+      publishYear: request.body.publishYear
+    }
+
+    const book = await Book.create(newBook)
+    return response.status(201).send(book)
+
+  } catch (error) {
+    console.log(error.message)
+    response.status(500).send({message: error.message})
+  }
+})
+
 // Route to PUT/PATCH one book by id
 // http://localhost:5555/books/14783495
 app.put('/books/:id', async (request, response) => {
@@ -91,6 +91,21 @@ app.put('/books/:id', async (request, response) => {
     }
     return response.status(200).send({message: 'Book updated successfully'})
 
+  } catch(error) {
+    console.log(error.message)
+    response.status(500).send({message: error.message})
+  }
+})
+
+// Route to PUT/PATCH one book by id
+app.delete('/books/:id', async (request, response) => {
+  try {
+    const {id} = request.params
+    const result = await Book.findByIdAndDelete(id);
+    if (!result) {
+      return response.status(404).json({message: 'Book not found'})
+    }
+    return response.status(200).json({message: 'Book deleted successfully'})
   } catch(error) {
     console.log(error.message)
     response.status(500).send({message: error.message})
