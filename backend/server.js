88b2d96a54067b91e0e9788815ab47f4a0a0a482
KenjaminButton <kennethpchang@gmail.com>
2023-09-02 23:11:25 +0800

    First HTTP Route created

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,11 @@ import {PORT} from './config.js'
 
 const app = express();
 
+app.get('/', (req, res) => {
+  console.log(req)
+  return res.status(234).send('Welcome to Kenjamin Button land')
+})
+
 app.listen(PORT, () => {
   console.log(`APP is listening to port: ${PORT}`)
 })
